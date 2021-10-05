3c7355d0d00901dc5aa6045e173d4fba61f63115
tua2 <18521606@gm.uit.edu.vn>
2021-10-05 12:44:44 +0700

    phan loai, js, tang toc

--- script.js
+++ script.js
@@ -9,3 +9,26 @@ for(let i=0;i<buttonsPagination.length;i++)
         this.classList.add("active");
     })
 }
+
+let list=document.querySelectorAll('.list');
+let item=document.querySelectorAll('.post');
+for(let i=0;i<list.length;i++){
+    list[i].addEventListener("click",function(){
+        for(let j=0;j<list.length;j++){
+            list[j].classList.remove("active");
+
+        }
+        this.classList.add("active");
+        let dataFilter=this.getAttribute('data-filter');
+        for(let t=0;t<item.length;t++){
+            item[t].classList.remove("active");
+            item[t].classList.add("hide");
+            console.log(dataFilter,item[t].getAttribute("data-item"))
+            if(item[t].getAttribute("data-item") ==dataFilter||dataFilter=="All")
+            {
+                item[t].classList.remove("hide");
+                item[t].classList.add("active");
+            }
+        }
+    })
+}
